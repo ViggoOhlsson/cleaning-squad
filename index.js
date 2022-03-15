de3ebede4acfdcfbf864dc0dba61505c3862a8d6
19OZMAN <76961722+19ozman@users.noreply.github.com>
2022-03-15 15:40:20 +0100

    token

--- index.js
+++ index.js
@@ -24,6 +24,23 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+	const { token } = req.cookies;
+
+	if (token && jwt.verify(token, process.env.JWT_SECRET)) {
+		const tokenData = jwt.decode(token, process.env.JWT_SECRET);
+		res.locals.loginInfo =
+			tokenData.username + " " + tokenData.userId + " " + tokenData.role;
+		res.locals.loginUser = tokenData.username;
+		res.locals.loginId = tokenData.userId;
+		res.locals.isLoggedIn = true;
+	} else {
+		res.locals.loginInfo = "not logged in";
+		res.locals.isLoggedIn = false;
+	}
+	next();
+});
+
 app.get("/", async (req, res) => {
 	res.send("Hello World!");
 });
