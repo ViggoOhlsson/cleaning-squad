58f576bafd957b2607221ffa8acf37940c04e953
PeppZZon <jespersimonlind@gmail.com>
2022-03-22 14:08:06 +0100

    Added user post, and validation

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -1,9 +1,9 @@
 const { Schema, model } = require("mongoose");
-const userSchema = new Schema({ 
-    username: { type: String, required: true },
-    password: {type:String, required:true },
-    email: {type:String, required:true},
-    address: {type:String, required:true }
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  address: { type: String, required: true },
 });
 const UserModel = model("Users", userSchema);
-module.exports = UserModel
+module.exports = UserModel;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,36 @@
 require("dotenv").config();
+require("../mongoose.js");
 const express = require("express");
 const router = express.Router();
-const { UserModel } = require("../models/UsersModel.js");
+const UserModel = require("../models/UsersModel.js");
 
 router.get("/registrera", (req, res) => {
   res.render("users/users-create");
 });
 
+router.post("/registrera", async (req, res) => {
+  const { username, password, confirmPassword, email, address } = req.body;
+  UserModel.findOne({ username }, async (err, user) => {
+    if (user) {
+      res.render("anvandare/users-create", {
+        error: "Username already exists",
+      });
+    } else if (password.length <= 3) {
+      res.render("users/users-create", {
+        error: "Your password must contain at least 4 characters",
+      });
+    } else if (password !== confirmPassword) {
+      res.render("users/users-create", { error: "Passwords don't match" });
+    } else {
+      const newUser = new UserModel({
+        username,
+        password,
+        email,
+        address,
+      });
+      await newUser.save();
+      res.redirect("/");
+    }
+  });
+});
 module.exports = router;
