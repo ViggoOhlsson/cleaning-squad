784c1603f29d0716815cebb23e5a7d180ad5c794
PeppZZon <jespersimonlind@gmail.com>
2022-03-29 15:21:59 +0200

    Added gdpr box and window

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -7,53 +7,57 @@ const UserModel = require("../models/UsersModel.js");
 const jwt = require("jsonwebtoken");
 
 router.get("/registrera", (req, res) => {
-	res.render("users/users-create");
+  res.render("users/users-create");
 });
 
 router.post("/registrera", async (req, res) => {
-	const { username, password, confirmPassword, email, address } = req.body;
-	UserModel.findOne({ username }, async (err, user) => {
-		if (user) {
-			res.render("anvandare/users-create", {
-				error: "Username already exists",
-			});
-		} else if (password.length <= 3) {
-			res.render("users/users-create", {
-				error: "Ditt lösenord måste vara minst 4 tecken långt!",
-			});
-		} else if (password !== confirmPassword) {
-			res.render("users/users-create", { error: "Lösenord matchar inte!" });
-		} else {
-			const newUser = new UserModel({
-				username,
-				password: utils.hashPassword(password),
-				email,
-				address,
-			});
-			await newUser.save();
-			res.redirect("/");
-		}
-	});
+  const { username, password, confirmPassword, email, address } = req.body;
+  UserModel.findOne({ username }, async (err, user) => {
+    if (user) {
+      res.render("anvandare/users-create", {
+        error: "Username already exists",
+      });
+    } else if (password.length <= 3) {
+      res.render("users/users-create", {
+        error: "Ditt lösenord måste vara minst 4 tecken långt!",
+      });
+    } else if (password !== confirmPassword) {
+      res.render("users/users-create", { error: "Lösenord matchar inte!" });
+    } else {
+      const newUser = new UserModel({
+        username,
+        password: utils.hashPassword(password),
+        email,
+        address,
+      });
+      await newUser.save();
+      res.redirect("/");
+    }
+  });
+});
+
+router.get("/anvandarvillkor", (req, res) => {
+  res.render("users/users-gdpr");
 });
 
 router.get("/logga-in", (req, res) => {
-	res.render("users/users-login");
+  res.render("users/users-login");
 });
 
 router.post("/logga-in", async (req, res) => {
-	const { username, password } = req.body;
+  const { username, password } = req.body;
 
-	UserModel.findOne({ username }, (err, user) => {
-		if (user && utils.comparePassword(password, user.password)) {
-			const userData = { userId: user._id, username };
-			const accessToken = jwt.sign(userData, process.env.JWT_SECRET);
-			res.cookie("token", accessToken);
-			console.log("Logga in lyckades");
-			res.redirect("/");
-		} else {
-			res.render("users/users-login", { error: "Login misslyckad" });
-		}
-	});
+  UserModel.findOne({ username }, (err, user) => {
+    if (user && utils.comparePassword(password, user.password)) {
+      const userData = { userId: user._id, username };
+      const accessToken = jwt.sign(userData, process.env.JWT_SECRET);
+      res.cookie("token", accessToken);
+      console.log("Logga in lyckades");
+      res.redirect("/");
+    } else {
+      res.render("users/users-login", { error: "Login misslyckad" });
+    }
+  });
 });
 
 module.exports = router;
