d6ab75fd4dbdad88907d6c0d8d7a052c08319dc3
PeppZZon <jespersimonlind@gmail.com>
2022-03-30 11:26:43 +0200

    Ändrade stavfel haha

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const adminsRouter = require("./routes/adminsRoute.js");
 const bookingsRouter = require("./routes/bookingsRouter.js");
 
 app.engine(
-	"hbs",
-	exphbs.engine({
-		defaultLayout: "main",
-		extname: ".hbs",
-		helpers: require("./helpers"),
-	})
+  "hbs",
+  exphbs.engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: require("./helpers"),
+  })
 );
 app.set("view engine", "hbs");
 app.use(express.static("public"));
@@ -30,32 +30,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-	const { token } = req.cookies;
+  const { token } = req.cookies;
 
-	if (token && jwt.verify(token, process.env.JWT_SECRET)) {
-		const tokenData = jwt.decode(token, process.env.JWT_SECRET);
-		res.locals.loginInfo =
-			tokenData.username + " " + tokenData.userId + " " + tokenData.type;
-		res.locals.loginUser = tokenData.username;
-		res.locals.loginId = tokenData.userId;
-		res.locals.isLoggedIn = true;
-		res.locals.loginType = tokenData.type;
-	} else {
-		res.locals.loginInfo = "Not logged in";
-		res.locals.isLoggedIn = false;
-	}
-	next();
+  if (token && jwt.verify(token, process.env.JWT_SECRET)) {
+    const tokenData = jwt.decode(token, process.env.JWT_SECRET);
+    res.locals.loginInfo =
+      tokenData.username + " " + tokenData.userId + " " + tokenData.type;
+    res.locals.loginUser = tokenData.username;
+    res.locals.loginId = tokenData.userId;
+    res.locals.isLoggedIn = true;
+    res.locals.loginType = tokenData.type;
+  } else {
+    res.locals.loginInfo = "Not logged in";
+    res.locals.isLoggedIn = false;
+  }
+  next();
 });
 
 app.use("/anvandare", usersRouter);
 app.use("/kontakt", contactsRouter);
 app.use("/admin", adminsRouter);
-app.use("/bookning", bookingsRouter);
+app.use("/bokning", bookingsRouter);
 
 app.get("/", (req, res) => {
-	res.render("home");
+  res.render("home");
 });
 
 app.listen(port, () => {
-	console.log(`Listening to http://localhost:${port}`);
+  console.log(`Listening to http://localhost:${port}`);
 });
