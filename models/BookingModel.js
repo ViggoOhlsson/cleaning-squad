365bc7ee11948bae00aaa837d4142514cb9b6a43
19OZMAN <76961722+19ozman@users.noreply.github.com>
2022-03-31 18:02:52 +0200

    tweaks

--- models/BookingModel.js
+++ models/BookingModel.js
@@ -4,22 +4,17 @@ const bookingSchema = new Schema({
 	user: { type: Schema.Types.ObjectId, required: true },
 	cleaner: { type: Schema.Types.ObjectId, required: true },
 	time: { type: Number, required: true, default: Date.now },
-	address: {type: String, required: true},
-	county: {type: String, required: true},
+	address: { type: String, required: true },
+	county: { type: String, required: true },
 	type: {
 		type: String,
 		required: true,
-		enum: [
-			"Basic Städning",
-			"Topp Städning",
-			"Diamant Städning",
-			"Fönstertvätt",
-		],
+		enum: ["Basic", "Toppklass", "Diamant", "Fönstertvätt"],
 	},
 	status: {
 		type: String,
 		required: true,
-		enum: ["Inte Utförd", "Utförd", "Godkänd", "Inte Godkänd"],
+		enum: ["Ej Utförd", "Utförd", "Godkänd", "Ej Godkänd"],
 	},
 });
 const BookingModel = model("bookings", bookingSchema);
--- routes/bookingsRouter.js
+++ routes/bookingsRouter.js
@@ -7,25 +7,24 @@ const mongoose = require("mongoose");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("bookings/bookings-create");
+	res.render("bookings/bookings-create");
 });
 
-router.post("/", async (req,res) => {
-  console.log(req.body)
-  req.body.time = new Date(req.body.date + " " + req.body.time)
-  console.log(req.body.time)
-  
-  req.body.status = "Inte Utförd"
-  req.body.type = "Fönstertvätt"
-  req.body.cleaner = await EmployeeModel.findOne({type: "employee"}).lean()
-  req.body.user = new mongoose.Types.ObjectId(res.locals.loginId)
+router.post("/", async (req, res) => {
+	console.log(req.body);
+	req.body.time = new Date(req.body.date + " " + req.body.time);
+	console.log(req.body.time);
 
-  console.log("after processing", req.body)
-  let booking = new BookingModel(req.body)
-  console.log("BookingModel:", booking)
-  await booking.save()
-  res.redirect("/bokning")
-})
+	req.body.status = "Inte Utförd";
 
+	req.body.cleaner = await EmployeeModel.findOne({ type: "employee" }).lean();
+	req.body.user = new mongoose.Types.ObjectId(res.locals.loginId);
+
+	console.log("after processing", req.body);
+	let booking = new BookingModel(req.body);
+	console.log("BookingModel:", booking);
+	await booking.save();
+	res.redirect("/anvandare/" + res.locals.loginId);
+});
 
 module.exports = router;
